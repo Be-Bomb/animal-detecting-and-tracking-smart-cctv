7765468ee15f40ba5f18dac4081890adf1f73833
kookeej <bong041@inu.ac.kr>
2022-05-30 09:45:34 +0900

    pi server modules

---
+++ pi-server/cam.py
@@ -0,0 +1,40 @@
+import socket
+import time
+from imutils.video import VideoStream
+import imagezmq
+import argparse
+import requests
+
+"""
+라즈베리파이 카메라를 사용할 때
+"""
+
+def main(args):
+    sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(args.ip))
+
+    rpi_name = socket.gethostname()  # send RPi hostname with each image
+
+    picam = VideoStream(usePiCamera=True).start()
+    time.sleep(2.0)  # allow camera sensor to warm up
+
+    while True:  # send images as stream until Ctrl-C
+        image = picam.read()
+        sender.send_image(rpi_name, image)
+        
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Raspberry pi passes its video capture to server"
+    )
+
+    parser.add_argument(
+        "--ip",
+        default=None,
+        type=str,
+        required=True,
+        help="server IP that we want to pass",
+    )
+
+    args = parser.parse_args()
+    main(args)
---
+++ pi-server/webcam.py
@@ -0,0 +1,39 @@
+import socket
+import time
+from imutils.video import VideoStream
+import imagezmq
+import argparse
+
+"""
+웹캠을 사용할 
+"""
+
+def main(args):
+    sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(args.ip))
+
+    rpi_name = socket.gethostname()  # send RPi hostname with each image
+
+    picam = VideoStream(src=0).start()
+    time.sleep(2.0)  # allow camera sensor to warm up
+
+    while True:  # send images as stream until Ctrl-C
+        image = picam.read()
+        sender.send_image(rpi_name, image)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Raspberry pi passes its video capture to server"
+    )
+
+    parser.add_argument(
+        "--ip",
+        default=None,
+        type=str,
+        required=True,
+        help="server IP that we want to pass",
+    )
+
+    args = parser.parse_args()
+
+    main(args)
